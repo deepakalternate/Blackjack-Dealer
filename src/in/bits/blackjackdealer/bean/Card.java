6144704d167214c9e40cd6416015199e3df0cbc5
deepakalternate <d3epak91@gmail.com>
2015-10-24 00:36:23 +0530

    Fixed message types.

--- src/in/bits/blackjackdealer/bean/Card.java
+++ src/in/bits/blackjackdealer/bean/Card.java
@@ -1,5 +1,34 @@
 package in.bits.blackjackdealer.bean;
 
+import java.util.Random;
+
 public class Card {
+    
+    Random randomizer;
+    private int minSuit = 1;
+    private int maxSuit = 4;
+    private int minValue = 1;
+    private int maxValue = 13;
+    private int suit;
+    private int value;
+    
+    public Card() {
+        suit = randomizer.nextInt((maxSuit - minSuit) + 1) + minSuit;
+        value = randomizer.nextInt((maxValue - minValue) + 1) + minValue;
+    }
+
+    /**
+     * @return the suit
+     */
+    public int getSuit() {
+        return suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
     
 }
