97a883f51d8b29e3f9f24ef53f372c6679dde361
deepakalternate <d3epak91@gmail.com>
2015-10-24 00:36:33 +0530

    Fixed message types.

--- src/in/bits/blackjackdealer/bean/Message.java
+++ src/in/bits/blackjackdealer/bean/Message.java
@@ -2,4 +2,72 @@ package in.bits.blackjackdealer.bean;
 
 public class Message {
     
+    private Type type;
+    private String user;
+    private String message;
+    private Card card;
+    
+    public Message(Type type, Card card, String user, String message) {
+        this.type = type;
+        this.user = user;
+        this.message = message;
+        this.card = card;
+    }
+
+    /**
+     * @return the type
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the card
+     */
+    public Card getCard() {
+        return card;
+    }
+
+    /**
+     * @param card the card to set
+     */
+    public void setCard(Card card) {
+        this.card = card;
+    }
+    
 }
